fc808df28a0683641e9556cecec433eadae59248
odysseeandi <andreaskrausser@googlemail.com>
2024-05-26 17:59:13 +0200

    neuer Versuch

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = "https://einkaufszettel-neu-backend.onrender.com/"
+const baseUrl = "https://einkaufszettel-neu-backend.onrender.com"
 
 const getAllToDo = (setToDo) => {
     axios
